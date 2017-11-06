83c089c8c97f81c81bff17b0a834282721a560e4
russellpipal <russell.pipal@gmail.com>
2017-11-06 15:11:55 -0600

    challenge #4 complete

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. I worked through this one with a custom character-shifting algorithm, then made another with the suggested solution.
 2. To do this, I used vim to save the hidden text, as Atom didn't seem to like it.
 3. Just used a slightly different regex. Also opened the file and read it in one step, I don't know if this is better.
+4. Initially did this one recursively, then changed to a for loop. I don't know which is better.
---
+++ python-challenge-4.py
@@ -0,0 +1,33 @@
+"""Python challenge #4."""
+
+import urllib.request
+import re
+
+
+def scan_html(code):
+    for i in range(400):
+        url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={}".format(code)
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode("utf-8")
+            print(html)
+            code = re.search(r'the next nothing is ([0-9]+)', html).group(1)
+            print(code)
+
+
+nothing = input("Enter a nothing: ")
+scan_html(nothing)
+
+
+# def scan_html(code, counter):
+#     if counter <= 400:
+#         url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={}".format(code)
+#         with urllib.request.urlopen(url) as response:
+#             html = response.read().decode("utf-8")
+#             print(html)
+#             next_html = re.search(r'the next nothing is ([0-9]+)', html).group(1)
+#             print(next_html)
+#             counter += 1
+#             scan_html(next_html, counter)
+
+# nothing = input("Enter a nothing: ")
+# scan_html(nothing, 0)
